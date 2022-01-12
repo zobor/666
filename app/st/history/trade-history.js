3d6f34487a0e551855c1e2851e4915b1f0495c10
zobor <zobor@douyu.tv>
2022-01-12 16:22:54 +0800

    UPD

--- app/st/history/trade-history.js
+++ app/st/history/trade-history.js
@@ -1,11 +1,18 @@
 const data = `
+#2022-01-12
+74.01 74.19 800
+74.21 74.78 1300
+
+
 #2022-01-11
 77.01 77.60 800
 76.90 77.30 800
 77.83 78.52 500
 
+
 #2022-01-07
 77.87 77.98 2000
+
 
 #2022-01-05
 77.2 78.00 900
@@ -49,22 +56,34 @@ const getFee = (total) => {
 
 const calc = (list) => {
   const content = [];
+  const days = {};
   list.map(item => {
     item.list.map(data => {
       const obj = {};
       const a = parseInt((data.s - data.b) * data.t);
-      obj.time = item.time.replace(/\d{4}-/, '');
+      const time = item.time.replace(/\d{4}-/, '');
+      obj.time = time;
       obj.flo = +float2Fix((data.s - data.b)/data.b, 2).replace(/%/, '');
       obj.s = data.t / 100;
       obj.fee = getFee(data.b * data.t);
       obj.p1 = a;
-      obj.p2 = a-obj.fee;
+      obj.p2 = a - obj.fee;
       content.unshift(obj);
+
+      if (!days[time]) days[time] = 0;
+      days[time] += obj.p2;
     });
   });
 
-  return content;
+  return {content, days};
 };
 
 
-console.table(calc(parseData(data)));
+const showData = calc(parseData(data));
+console.table(showData.content);
+
+console.table(Object.keys(showData.days).map(day => ({time: day, pro: showData.days[day]})));
+
+const total = showData.content.reduce((aac, current) => current.p2 + aac,0);
+
+console.log('total', total);
